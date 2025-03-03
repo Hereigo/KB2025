75e57ef28a59bd4576056ca7afc04d27576847fd
Hereigo <hereigo@online.ua>
2025-03-03 21:19:38 +0200

    clean architecture

---
+++ CLEAN_ARCH/Domain/RequestHeader.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain
+{
+    public enum ReqHeadFieldType
+    {
+        Accept,
+        Crawler,
+        Device,
+        Encode,
+        Language,
+        Referer,
+        UsrAgent,
+    }
+
+    public class RequestHeader
+    {
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public ReqHeadFieldType Field { get; set; }
+
+        public string Text { get; set; }
+    }
+}

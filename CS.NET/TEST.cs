4c02d0a0ebfdcd092e159e7a108c5b801163be2d
Hereigo <hereigo@online.ua>
2026-01-22 13:44:03 +0200

    update

--- CS.NET/TEST.cs
+++ CS.NET/TEST.cs
@@ -1,4 +1,24 @@
+// - if - dotnet --version >= 10.x you can:
 // dotnet run TEST.cs
-// if - dotnet --version ==> 10.x
 
-Console.WriteLine($"Today is {DateTime.Now:dddd, MMM dd yyyy}");
+// - To create a Project you can:
+// dotnet project convert TEST.cs -o TEST.csproj
+
+#:sdk Microsoft.NET.Sdk.Web
+#:package Microsoft.AspNetCore.OpenApi@10.*-*
+#:package Humanizer@2.14.1
+
+using Humanizer;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var dotNet9Released = DateTimeOffset.Parse("2024-12-03");
+var since = DateTimeOffset.Now - dotNet9Released;
+var outputString = $"It has been {since.Humanize()} since .NET 9 was released.";
+
+app.MapGet("/", () => outputString);
+app.Run();

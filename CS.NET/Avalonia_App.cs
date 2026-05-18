579528e242748e5c86c130cc3f0c22fa69723f45
Hereigo <hereigo@online.ua>
2026-05-18 21:04:20 +0300

    update

---
+++ CS.NET/Avalonia_App.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Themes.Simple;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        AppBuilder
+            .Configure<Application>()
+            .UsePlatformDetect()
+            .Start(AppMain, args);
+    }
+
+    public static void AppMain(Application app, string[] args)
+    {
+        // Application needs a theme to render window content
+        app.Styles.Add(new SimpleTheme());
+        app.RequestedThemeVariant = Avalonia.Styling.ThemeVariant.Default; // Default, Dark, Light
+
+        // Create window
+        var win = new Window();
+        win.Title = "Avalonia no XAML";
+        win.Width = 800;
+        win.Height = 600;
+
+        var text = new Label();
+        win.Content = text;
+
+        text.Content = "Hello from C#!";
+        text.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center;
+        text.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center;
+        text.FontSize = 72;
+
+        win.Show();
+        app.Run(win);
+    }
+}

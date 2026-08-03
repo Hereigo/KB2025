ced030c91a9f550d65bba37c40558cedef632ddc
Hereigo <hereigo@online.ua>
2026-08-03 21:43:09 +0300

    update

--- NODEJS/fs/001_file_reading.js
+++ NODEJS/fs/001_file_reading.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 
+try {
+  console.log('File read sync #1');
+  const data = fs.readFileSync('example.txt', 'utf8');
+  console.log('File content (sync):', data);
+} catch (err) {
+  console.error('Error reading file (sync):', err);
+}
+
+console.log('File read async');
 fs.readFile('example.txt', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
   }
-  console.log('File content:', data);
+  console.log('File content (async):', data);
 });
 
 try {
+  console.log('File read sync #2');
   const data = fs.readFileSync('example.txt', 'utf8');
   console.log('File content (sync):', data);
 } catch (err) {

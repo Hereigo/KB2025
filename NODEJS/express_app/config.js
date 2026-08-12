19ffd0a6c89df38aafa85390fe92ec2051820b7e
Hereigo <hereigo@online.ua>
2026-08-12 21:13:35 +0300

    update

--- NODEJS/express_app/config.js
+++ NODEJS/express_app/config.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const PUBLIC_PORT = process.env.PUBLIC_PORT;
 export const WEB_TOKEN_SECRET_KEY = process.env.WEB_TOKEN_SECRET_KEY;
 export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY;
+export const MONGODB_URI = process.env.MONGODB_URI;
